72920fc8b7084290321d68864e0ea548a85f8368
Ron Chan <ksc.ron@gmail.com>
2015-08-10 16:59:59 +0800

    update Ron 08/10-16:59

---
+++ wp-content/themes/omega/template-news.php
@@ -0,0 +1,6 @@
+<?php
+/*
+Template Name: News
+*/
+?>
+<?php get_template_part('templates/content', 'news'); ?>
---
+++ wp-content/themes/omega/templates/content-news.php
@@ -0,0 +1,46 @@
+<div class="container">
+	<div class="row">
+		<div class="innerContentContainer">
+            <div class="mainPost">
+                <div class="col-sm-8 feature-img-container">
+                    <img src="<?=get_stylesheet_directory_uri()?>/assets/img/news/img_news_dummy_1.jpg" class="img-responsive" />
+                    <div class="feature-img-caption">Image Credit: UPI Photo Collection. United Press International, 2010. Tech Arts Collection. Web. 1 May 2012.</div>
+                </div>
+                <div class="col-sm-4 feature-content-container">
+                    <div class="feature-content-inner">
+                        <p class="post-date">june 20, 2015</p>
+                        <h2>An achievement of unlimited horizons</h2>
+                        <p class="post-excerpt">The Panda factory wins WCM Gold. The objective of which is constant improvement of performance in the production process by eliminating waste and improving quality, safety, people's working conditions and respect for the environment.</p>
+                        <a href="#" class="readmore_link">READ MORE ></a>
+                    </div>
+                </div>
+            </div>
+            <div class="container subPost-container">
+                <div class="col-sm-4 subPostItem">
+                    <img src="<?=get_stylesheet_directory_uri()?>/assets/img/news/img_news_dummy_2.jpg" class="img-responsive" />
+                    <p class="img-caption">Image Credit: UPI Photo Collection. United Press International, 2010. Tech Arts Collection. Web. 1 May 2012.</p>
+                    <p class="post-date">june 20, 2015</p>
+                    <h2>An achievement of unlimited horizons</h2>
+                    <p class="post-excerpt">The Panda factory wins WCM Gold. The objective of which is constant improvement of performance in the production process by eliminating waste and improving quality, safety, people's working conditions and respect for the environment.</p>
+                    <a href="#" class="readmore_link">READ MORE ></a>
+                </div>
+                <div class="col-sm-4 subPostItem">
+                    <img src="<?=get_stylesheet_directory_uri()?>/assets/img/news/img_news_dummy_3.jpg" class="img-responsive" />
+                    <p class="img-caption">Image Credit: UPI Photo Collection. United Press International, 2010. Tech Arts Collection. Web. 1 May 2012.</p>
+                    <p class="post-date">june 20, 2015</p>
+                    <h2>An achievement of unlimited horizons</h2>
+                    <p class="post-excerpt">The Panda factory wins WCM Gold. The objective of which is constant improvement of performance in the production process by eliminating waste and improving quality, safety, people's working conditions and respect for the environment.</p>
+                    <a href="#" class="readmore_link">READ MORE ></a>
+                </div>
+                <div class="col-sm-4 subPostItem">
+                    <img src="<?=get_stylesheet_directory_uri()?>/assets/img/news/img_news_dummy_4.jpg" class="img-responsive" />
+                    <p class="img-caption">Image Credit: UPI Photo Collection. United Press International, 2010. Tech Arts Collection. Web. 1 May 2012.</p>
+                    <p class="post-date">june 20, 2015</p>
+                    <h2>An achievement of unlimited horizons</h2>
+                    <p class="post-excerpt">The Panda factory wins WCM Gold. The objective of which is constant improvement of performance in the production process by eliminating waste and improving quality, safety, people's working conditions and respect for the environment.</p>
+                    <a href="#" class="readmore_link">READ MORE ></a>
+                </div>
+            </div>
+		</div>
+	</div>
+</div>
