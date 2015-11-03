f43d71d0f24a938b6eff68b222bf0de6b5136575
SedgwickRichardson <michiel@sedgwick-richardson.com>
2015-11-03 14:30:05 +0800

    after pull

--- wp-content/themes/omega/lib/extras.php
+++ wp-content/themes/omega/lib/extras.php
@@ -6,3 +6,8 @@ function roots_excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'roots') . '</a>';
 }
 add_filter('excerpt_more', 'roots_excerpt_more');
+
+function wpbeginner_remove_version() {
+	return '';
+}
+add_filter('the_generator', 'wpbeginner_remove_version');
