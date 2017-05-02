487ec7be3b49ab261c57f1aa42f30c82a0a22b24
Ron Chan <ksc.ron@gmail.com>
2017-05-02 22:52:28 +0800

    update Ron 20170502-22:52

--- wp-content/themes/omega/templates/content-home.php
+++ wp-content/themes/omega/templates/content-home.php
@@ -2,38 +2,30 @@
 	<div class="row">
         <div class="homeBannerContainer">
             <div class="homeBannerItemContainer clearfix" id="homeBannerItemContainer">
-                <div class="homeBannerItem" style="z-index:999;"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/banner_1.png" alt="" class="img-responsive" /></div>
+                <div class="homeBannerItem" style="z-index:999;"><img src="<?=the_post_thumbnail_url( 'full' )?>" alt="" class="img-responsive" /></div>
                 <a href="javascript:;" class="homeBannerBtnPrev"></a>
                 <a href="javascript:;" class="homeBannerBtnNext"></a>
                 <div class="homeThreeColumnContainer clearfix container">
                 	<div class="row">
-                        <div class="homeThreeColumnItem col-sm-4">
-                        	<div class="homeThreeColumnItemInnerDiv">
-                                <div class="threeColumnHeader">SUPPLY CHAIN MONITORING</div>
-                                <div class="threeColumnContent supplyChainMonitoring">
-                                    Every day many of the world’s most trusted consumer brands put their faith in Omega to ensure the integrity of their global supply chains.
-                                    <div class="learnMoreContainer"><a href="<?=site_url()?>/services/auditing/">LEARN MORE &gt;</a></div>
-                                </div>
-                           	</div>
-                        </div>
-                        <div class="homeThreeColumnItem col-sm-4">
-                        	<div class="homeThreeColumnItemInnerDiv">
-                                <div class="threeColumnHeader">ethical behavior</div>
-                                <div class="threeColumnContent ethicalBehavior">
-                                    A dedicated, internal ethical team requires our staff to uphold exceptional levels of integrity, honesty and transparency at all times.
-                                    <div class="learnMoreContainer"><a href="<?=site_url()?>/ethics/">LEARN MORE &gt;</a></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="homeThreeColumnItem col-sm-4">
-                        	<div class="homeThreeColumnItemInnerDiv">
-                                <div class="threeColumnHeader">a different approach</div>
-                                <div class="threeColumnContent differntApproach">
-                                    Omega goes beyond monitoring. Our focus is on thorough investigations, complete inspections, clear communication and effective remediation.
-                                    <div class="learnMoreContainer"><a href="<?=site_url()?>/about-us/">LEARN MORE &gt;</a></div>
-                                </div>
-                           	</div>
-                        </div>
+                    	<?php
+						
+							if( have_rows('three_column') ):
+								 // loop through the rows of data
+								while ( have_rows('three_column') ) : the_row();
+									if( get_row_layout() == 'column_block' ):?>
+                                        <div class="homeThreeColumnItem col-sm-4">
+                                            <div class="homeThreeColumnItemInnerDiv">
+                                                <div class="threeColumnHeader"><?php the_sub_field('title'); ?></div>
+                                                <div class="threeColumnContent <?php the_sub_field('icon'); ?>">
+                                                    <?php the_sub_field('content'); ?>
+                                                    <div class="learnMoreContainer"><a href="<?=get_permalink(the_sub_field('learn_more_link'));?>">LEARN MORE &gt;</a></div>
+                                                </div>
+                                            </div>
+                                        </div>
+								<?	endif;
+								endwhile;
+							endif;
+						?>
                 	</div>
                 </div>
             </div>
